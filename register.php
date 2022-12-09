26d7d77606e2cfd601641d9806b0c2030b73d5fe
VeXiz <100276695+VeXiz@users.noreply.github.com>
2022-12-09 15:29:25 +0330

    title

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-        <meta charset="utf-8" />   <title>RezDom | ورود</title>
+        <meta charset="utf-8" />   <title>RezDom | ثبت نام</title>
         <meta name="author" content="AmirKhani" />
         <!-- favicon -->
         <link rel="shortcut icon" href="images/a_add.png">
