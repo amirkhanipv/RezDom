33ef3bad66b4c724fc6f204ac3a97f4aaa26d250
amirkhanipv <120177905+amirkhanipv@users.noreply.github.com>
2022-12-09 21:51:43 +0330

    second panel change

--- panel.php
+++ panel.php
@@ -16,6 +16,8 @@ $user = $DB->CheckAcc($UName);
 $_UserName = $user->UserName;
 $_FirstName = $user->FirstName;
 $_LastName = $user->LastName;
+$_mix=$_FirstName." ".$_LastName;
+$_email=$user->Email;
 
 ?>
 <!DOCTYPE html>
@@ -55,12 +57,7 @@ $_LastName = $user->LastName;
                                     <div class="col-lg-10 col-md-9">
                                         <div class="row align-items-end">
                                             <div class="col-md-7 text-md-start text-center mt-4 mt-sm-0">
-                                                <h3 class="title mb-0">جعفر عباسی</h3>
-                                                <small class="text-muted h6 me-2">توسعه وب</small>
-                                                <ul class="list-inline mb-0 mt-3">
-                                                    <li class="list-inline-item me-2"><a href="javascript:void(0)" class="text-muted" title="اینستاگرام"><i data-feather="instagram" class="fea icon-sm me-2"></i>jafar_abbasi</a></li>
-                                                    <li class="list-inline-item"><a href="javascript:void(0)" class="text-muted" title="لینکدین"><i data-feather="linkedin" class="fea icon-sm me-2"></i>جعفر عباسی</a></li>
-                                                </ul>
+                                                <h3 class="title mb-0"><?php echo $_mix; ?></h3>
                                             </div><!--end col-->
                                             <div class="col-md-5 text-md-end text-center">
                                                 <ul class="list-unstyled social-icon social mb-0 mt-4">
@@ -79,7 +76,7 @@ $_LastName = $user->LastName;
         </section><!--end section-->
         <!-- Hero End -->
 
-        <section style="padding-bottom: 100px;">
+        <section style="padding-bottom: 100px; ">
             
             <div class="container mt-100 mt-60">
                 <div class="row">
@@ -161,16 +158,6 @@ $_LastName = $user->LastName;
                             <div class="tab-pane fade show active" id="dash-board" role="tabpanel" aria-labelledby="dashboard">
                                 
                                 <h5 class="text-md-start text-center">مشخصات شخصی</h5>
-
-                                <div class="mt-3 text-md-start text-center d-sm-flex">
-                                    <img src="images/client/05.jpg" class="avatar float-md-left avatar-medium rounded-circle shadow me-md-4" alt="">
-                                    
-                                    <div class="mt-md-4 mt-3 mt-sm-0">
-                                        <a href="javascript:void(0)" class="btn btn-primary mt-2">تغییر تصویر</a>
-                                        <a href="javascript:void(0)" class="btn btn-outline-primary mt-2 ms-2">حذف </a>
-                                    </div>
-                                </div>
-
                                 <form>
                                     <div class="row mt-4">
                                         <div class="col-md-6">
@@ -196,7 +183,7 @@ $_LastName = $user->LastName;
                                                 <label class="form-label">ایمیل شما </label>
                                                 <div class="form-icon position-relative">
                                                     <i data-feather="mail" class="fea icon-sm icons"></i>
-                                                    <input name="email" id="email" type="email" class="form-control ps-5" placeholder="ایمیل شما :">
+                                                    <input name="email" id="email" type="email" class="form-control ps-5" placeholder="ایمیل شما :" value="<?php echo($_email)?>">
                                                 </div>
                                             </div> 
                                         </div><!--end col-->
@@ -205,7 +192,7 @@ $_LastName = $user->LastName;
                                                 <label class="form-label">نام کاربری</label>
                                                 <div class="form-icon position-relative">
                                                     <i data-feather="user" class="fea icon-sm icons"></i>
-                                                    <input name="name" id="occupation" type="text" class="form-control ps-5" placeholder="نام کاربری :">
+                                                    <input name="name" id="occupation" type="text" class="form-control ps-5" placeholder="نام کاربری :" value="<?php echo($_UserName)?>">
                                                 </div>
                                             </div> 
                                         </div><!--end col-->
